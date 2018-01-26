937c494458a814a89be0b2ae0cb66a9ec11128f4
Jose <jrosales0001@gmail.com>
2018-01-26 15:24:57 -0500

    consonant translation

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -11,6 +11,7 @@ namespace Lab6
         static void Main(string[] args)
         {
             Console.WriteLine("Pig Latin Translator");
+
             string OriginalString = ReadUserInput("\nPlease enter a word:");
             char [] LowerCaseStringArray = OriginalString.ToLower().ToCharArray();
 
@@ -19,15 +20,34 @@ namespace Lab6
             if ("aeiou".Contains(LowerCaseString[0]))
             {
                 Console.WriteLine("\nTranslation: {0}way", LowerCaseString);
-            }            
-            //char[] VowelArray = {'a','e','i','o','u','A','E','I','O','U'};            
+            }
+            else
+            {
+                string PigLatinConsonant = ConsonantTranslation(LowerCaseString);
+               
+                Console.WriteLine("\nTranslation: {0}", PigLatinConsonant);
 
+            }
         }
         public static string ReadUserInput (string UserPrompt)
         {
             Console.WriteLine(UserPrompt);
             string OriginalString = Console.ReadLine();
             return OriginalString;
-        }        
+        }
+
+        public static string ConsonantTranslation(string LowerCaseString)
+        {
+            char[] VowelArray = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            int VowelIndex = LowerCaseString.IndexOfAny(VowelArray);
+
+            string BeginningConsonants = LowerCaseString.Substring(0, VowelIndex);
+            string RestOfWord = LowerCaseString.Substring(VowelIndex);
+
+            string TranslatedWord = RestOfWord + BeginningConsonants + "ay";
+
+            return TranslatedWord;
+        }
     }
 }
